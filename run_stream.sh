e6d2b359fde74c067852f1890c03bc5be4455c13
Kyle Gerard Felker <kfelker@princeton.edu>
2018-02-05 16:26:41 -0500

    Add driver Bash script to loop over OMP thread count for STREAM

---
+++ run_stream.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=28
+#SBATCH -t 10:00
+
+#module load intel
+#make stream icc
+
+for nthreads in 1 2 # 3 4 5 6 7 8
+do
+    export OMP_NUM_THREADS=$nthreads
+    ./stream.omp.AVX2.18M.20x.icc
+done
