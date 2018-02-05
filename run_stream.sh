668db821f80f7bc2c95f847cf28719c444f6ab8f
Kyle Gerard Felker <kfelker@princeton.edu>
2018-02-05 17:55:56 -0500

    Extend driver script to process ASCII output into CSV

--- run_stream.sh
+++ run_stream.sh
@@ -6,8 +6,16 @@
 module load intel
 #make stream icc
 
-for nthreads in 1 2 # 3 4 5 6 7 8
+computer_name='perseus'
+max_threads=28
+stream_summary_filename="${computer_name}_STREAM_results.txt"
+
+echo "#n copy                              scale                              add                                triad" >> ${stream_summary_filename}
+echo "#Best MB/s Avg time Min time Max time" >> ${stream_summary_filename}
+for nthreads in $(seq 1 ${max_threads})
 do
     export OMP_NUM_THREADS=$nthreads
-    ./stream.omp.AVX2.18M.20x.icc
+    ./stream.omp.AVX2.18M.20x.icc 2>&1 | tee bare_STREAM_output.txt
+    echo -n "${nthreads} " >> ${stream_summary_filename}
+    tail -n 7 bare_STREAM_output.txt | head -n 4 | sed 's/[^.0-9][^.0-9]*/ /g' | xargs  >> ${stream_summary_filename}
 done
